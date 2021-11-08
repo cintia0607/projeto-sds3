63ceeb8f94e8f3f62f58e31c92a30c893f9c175d
cintia leticia rodrigues <cintialeticia2018@gmail.com>
2021-11-07 23:39:32 -0300

    Env var

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import Pagination from "components/pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sales";
-import { isTemplateExpression } from "typescript";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/request";
 
