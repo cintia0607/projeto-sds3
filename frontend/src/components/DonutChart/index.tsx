12c02f513f0ac1dc349d1862af91737b6e91fed8
cintia leticia rodrigues <cintialeticia2018@gmail.com>
2021-11-07 22:03:00 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -22,7 +22,6 @@ const DonutChart = () => {
         const mySeries = data.map(x => x.sum);
 
         setChartData({ labels: myLabels, series: mySeries });
-        console.log(chartData)
       });
   }, []);
 
